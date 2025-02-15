b3544644e4a1ec677d8a031ca2b4078f3adc982e
himanushi <himanushi@users.noreply.github.com>
2025-02-15 16:46:55 +0900

    Boxコンポーネントのプロパティにショートハンドを追加

--- lib/components/box/Box.tsx
+++ lib/components/box/Box.tsx
@@ -1,5 +1,42 @@
 import { Ui, type UiProps } from "~/core";
+import { resolveShorthandProps } from "~/utils/resolveShorthandProps";
+
+const shortHands = {
+  itemsCenter: { alignItems: "center" },
+  itemsStart: { alignItems: "flex-start" },
+  itemsEnd: { alignItems: "flex-end" },
+  justifyCenter: { justifyContent: "center" },
+  justifyStart: { justifyContent: "flex-start" },
+  justifyEnd: { justifyContent: "flex-end" },
+};
+
+const defaultProps = {
+  display: "flex",
+};
 
 export const Box = (props: UiProps) => {
-  return <Ui {...props} />;
+  const mergedProps = { ...defaultProps, ...props };
+  const newProps = resolveShorthandProps(mergedProps, shortHands);
+  return (
+    <Ui
+      color="gray-900"
+      backgroundColor={{
+        xs: "",
+        sm: "",
+        md: "red",
+        lg: "blue",
+        xl: "green",
+      }}
+      border={{
+        xs: "1px solid",
+        sm: "2px solid",
+        md: "3px solid",
+        lg: "4px solid",
+        xl: "5px solid",
+      }}
+      borderColor="gray-900"
+      width={{ lg: "35px" }}
+      {...newProps}
+    />
+  );
 };
