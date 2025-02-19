22237c876e0a12f854620a5c8c04830b64874932
himanushi <himanushi@users.noreply.github.com>
2025-02-19 18:45:23 +0900

    Boxコンポーネントの削除

--- lib/components/index.ts
+++ lib/components/index.ts
@@ -1,4 +1,3 @@
-export * from "./box";
 export * from "./button";
 export * from "./checkbox";
 export * from "./icon";
--- lib/core/ui/Ui.tsx
+++ lib/core/ui/Ui.tsx
@@ -7,13 +7,26 @@ import type { BaseUiProps } from "~/utils/styleProcessor";
 import { BaseUi } from "./BaseUi";
 
 export const shortHands = {
-  iCenter: { alignItems: "center" },
-  iStart: { alignItems: "flex-start" },
-  iEnd: { alignItems: "flex-end" },
   jCenter: { justifyContent: "center" },
   jStart: { justifyContent: "flex-start" },
   jEnd: { justifyContent: "flex-end" },
   jBetween: { justifyContent: "space-between" },
+  jStretch: { justifyContent: "stretch" },
+
+  jSelfCenter: { justifySelf: "center" },
+  jSelfStart: { justifySelf: "flex-start" },
+  jSelfEnd: { justifySelf: "flex-end" },
+  jSelfStretch: { justifySelf: "stretch" },
+
+  iCenter: { alignItems: "center" },
+  iStart: { alignItems: "flex-start" },
+  iEnd: { alignItems: "flex-end" },
+  iStretch: { alignItems: "stretch" },
+
+  iSelfCenter: { alignSelf: "center" },
+  iSelfStart: { alignSelf: "flex-start" },
+  iSelfEnd: { alignSelf: "flex-end" },
+  iSelfStretch: { alignSelf: "stretch" },
 
   col: { flexDirection: "column" },
   wrap: { flexWrap: "wrap" },
@@ -40,6 +53,8 @@ export const shortHands = {
 
   absolute: { position: "absolute" },
   relative: { position: "relative" },
+
+  selectNone: { userSelect: "none" },
 } as const satisfies ShortHandType;
 
 const defaultProps = {
@@ -54,8 +69,9 @@ export type UiProps<E extends React.ElementType = "div"> = WithShorthandProps<
 
 /**
  * @shorthands
- * iCenter, iStart, iEnd, jCenter, jStart, jEnd, jBetween, col,
- * flexWrap, p, pt, pr, pb, pl, py, px, w, h, solid, radius, absolute, relative
+ * jCenter, jStart, jEnd, jBetween, jStretch, jSelfCenter, jSelfStart, jSelfEnd, jSelfStretch,
+ * iCenter, iStart, iEnd, iStretch, iSelfCenter, iSelfStart, iSelfEnd, iSelfStretch,
+ * col, wrap, p, pt, pr, pb, pl, py, px, w, h, solid, radius, absolute, relative, selectNone
  */
 export const Ui = <E extends React.ElementType = "div">(props: UiProps<E>) => {
   const mergedProps = { ...defaultProps, ...props };
